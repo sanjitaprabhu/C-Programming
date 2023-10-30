5988f9b177f1acb7815c3cc5b7c8b7f238ae1ad0
Sanjita Prabhu <140255687+sanjitaprabhu@users.noreply.github.com>
2023-10-30 21:22:42 +0530

    Update grade.cpp

--- week 2/grade.cpp
+++ week 2/grade.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-	float p,c,b,m,cs;
+	float p,c,b,m,cs,sum,per;
 	printf("Enter marks of Physics: ");
 	scanf("%f",&p);
 	printf("Enter marks of Chemistry: ");
@@ -13,67 +13,19 @@ int main()
 	printf("Enter marks of Computer: ");
 	scanf("%f",&cs);
 	
-	if (p>=90)
-	printf("Grade A\n");
-	else if (p>=80)
-	printf("Grade B\n");
-	else if (p>=70)
-	printf("Grade C\n");
-	else if (p>=60)
-	printf("Grade D\n");
-	else if (p>=40)
-	printf("Grade E\n");
-	else
-	printf("Grade F\n");
+	sum=p+c+b+m+cs;
+	per=sum/5;
+	printf("%f \n",per);
 	
-	if (c>=90)
+	if (per>=90)
 	printf("Grade A\n");
-	else if (c>=80)
+	else if (per>=80)
 	printf("Grade B\n");
-	else if (c>=70)
+	else if (per>=70)
 	printf("Grade C\n");
-	else if (c>=60)
+	else if (per>=60)
 	printf("Grade D\n");
-	else if (c>=40)
-	printf("Grade E\n");
-	else
-	printf("Grade F\n");
-	
-	if (b>=90)
-	printf("Grade A\n");
-	else if (b>=80)
-	printf("Grade B\n");
-	else if (b>=70)
-	printf("Grade C\n");
-	else if (b>=60)
-	printf("Grade D\n");
-	else if (b>=40)
-	printf("Grade E\n");
-	else
-	printf("Grade F\n");
-	
-	if (m>=90)
-	printf("Grade A\n");
-	else if (m>=80)
-	printf("Grade B\n");
-	else if (m>=70)
-	printf("Grade C\n");
-	else if (m>=60)
-	printf("Grade D\n");
-	else if (m>=40)
-	printf("Grade E\n");
-	else
-	printf("Grade F\n");
-	
-	if (cs>=90)
-	printf("Grade A\n");
-	else if (cs>=80)
-	printf("Grade B\n");
-	else if (cs>=70)
-	printf("Grade C\n");
-	else if (cs>=60)
-	printf("Grade D\n");
-	else if (cs>=40)
+	else if (per>=40)
 	printf("Grade E\n");
 	else
 	printf("Grade F\n");
