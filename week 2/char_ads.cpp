56237c76f19d1acf2e330222f553e39acc44d46f
Sanjita Prabhu <140255687+sanjitaprabhu@users.noreply.github.com>
2023-10-31 13:29:26 +0530

    Week 2

---
+++ week 2/char_ads.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	char a;
+	printf("Enter character: ");
+	scanf("%c",&a);
+	if (('a'<=a && a<='z')||('A'<=a && a<='Z'))
+		printf("Alphabet");
+	else if ('0'<=a && a<='9')
+		printf("Digit");
+	else
+		printf("Special Character");
+}
